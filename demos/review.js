5427dc5a9a2d0d371e10d173f93db6a14eb44a90
Bryan <bmiller@tb.yearup.org>
2022-10-31 13:37:23 -0400

    demo review.js

---
+++ demos/review.js
@@ -0,0 +1,34 @@
+
+text = "Now is the time"
+//get last letter
+console.log(text[text.length - 1]);
+
+//print all but the last letter - loop or substr/slice
+
+//list = text.split("") //convert to array from string example 1
+list = [...text]; //convert to array from string example 2 using newer ES6
+
+list.forEach((e, i) => {
+    if (i < text.length - 1) {
+        console.log(e)
+    }
+});
+
+//convert back into a string
+//reverse a list
+output = "" 
+for(i=text.length-1; i>=0; i--){
+   output += text[i];
+}
+console.log(output);
+
+console.log([...text].reverse().join("")); //join converts an array back to string
+
+//reverse the words i a string
+console.log(text.split("").reverse().join(" ")); 
+
+//TERNARY OPERATOR IS A ONE LINE IF/ELSE
+function isOfAgeMessage(age){
+    return age >= 21 ? "YES" : "NO";
+}
+console.log(isOfAgeMessage(99));
