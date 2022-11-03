b937c5f6bbd72d13b415196f1b7d2195eeaae9cd
Bryan <bmiller@tb.yearup.org>
2022-11-03 14:10:14 -0400

    rolling rocks

---
+++ demos/rolling_rocks.js
@@ -0,0 +1,25 @@
+/*
+rolling rocks 
+Rules:
+    rocks roll left to right
+    Walls fo not roll
+    Walls stop rocks from rolling
+    Stopped rocks stop their neighboring rocks from rolling
+
+    O is a rock
+    X is a wall
+    . is empty space
+
+    The world is aseries of symbols that change over time.
+    Time liips through a series of cycles until they stop changing(become stable). (at most 8)
+
+    here are some initial inputs and final outputs (we are omiting the states in between)
+    "O......X" becomes "......OX"
+    ".O.X.O.X" becomes "..OX..OX"
+    "OOO..XO." becomes "..OOOX.."
+*/
+function roll(initial){
+    final = initial
+    //...
+    return final
+}
